4867c3d09e661babeb9afc7551dada8bcf473075
Kevin Renskers <kevin@loopwerk.io>
2022-09-15 16:29:17 +0200

    Use provided fetch function

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,10 +1,9 @@
 import { redirect } from "@sveltejs/kit";
 import type { LayoutLoad } from "./$types";
 
-export const load: LayoutLoad = async ({ data }) => {
+export const load: LayoutLoad = async ({ fetch, data }) => {
   if (data.token) {
-    // Why does a relative URL not work here???
-    await fetch("http://127.0.0.1:5173/auth/logout", { method: "POST" });
+    await fetch("/auth/logout", { method: "POST" });
     throw redirect(307, "/");
   }
 
